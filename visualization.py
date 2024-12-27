97b4862dd0780587344fda7895aed396b1820bd7
amas2025 <amashypermarket@gmail.com>
2024-12-27 17:03:33 +0300

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -9,8 +9,16 @@ def display_ebooks_online(books):
             base64_pdf = base64.b64encode(file.read()).decode('utf-8')
             pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="500"></iframe>'
 
-        # Button to read the book inline
-        if st.button(f"Read {book['title']}", key=f"read_button_{idx}"):
+        # Add download button
+        st.download_button(
+            label="Download",
+            data=open(book['file'], "rb").read(),
+            file_name=os.path.basename(book['file']),
+            mime="application/pdf",
+        )
+
+        # Add "Read Online" button below the download button
+        if st.button(f"Read Online", key=f"read_online_{idx}"):
             st.markdown(pdf_display, unsafe_allow_html=True)
 
         # Add a horizontal divider between books
