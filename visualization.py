c7780039519e97f75e4872d11b22e2ec13e20842
amas2025 <amashypermarket@gmail.com>
2024-12-27 17:01:31 +0300

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -3,14 +3,14 @@ import base64
 import os
 
 def display_ebooks_online(books):
-    for book in books:
+    for idx, book in enumerate(books):
         st.markdown(f"### {book['title']}")
         with open(book['file'], "rb") as file:
             base64_pdf = base64.b64encode(file.read()).decode('utf-8')
             pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="500"></iframe>'
 
         # Button to read the book inline
-        if st.button(f"Read {book['title']}"):
+        if st.button(f"Read {book['title']}", key=f"read_button_{idx}"):
             st.markdown(pdf_display, unsafe_allow_html=True)
 
         # Add a horizontal divider between books
